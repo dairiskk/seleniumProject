773411467a3d19da6e9f7d852f01681d06708c58
DairisKlesniks <dairis.klesniks@28stone.com>
2019-11-11 20:10:47 +0200

    initial commit

---
+++ src/test/java/Steps/MyStepdefs.java
@@ -0,0 +1,23 @@
+package Steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class MyStepdefs {
+    @Given("today is Sunday")
+    public void todayIsSunday() {
+        Assert.assertEquals("TEST","TEST");
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void iAskWhetherItSFridayYet() {
+        Assert.assertEquals("TEST","TEST");
+    }
+
+    @Then("I should be told {string}")
+    public void iShouldBeTold(String arg0) {
+        Assert.assertEquals("TEST","TEST");
+    }
+}
